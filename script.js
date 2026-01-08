f831c52c9705571f47bbc1528bcdc4a548ca2e83
Vikramjeet Singh <123534640+vikramjeetthind@users.noreply.github.com>
2026-01-08 22:46:58 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function filterProjects(type){
-  const projects=document.querySelectorAll('.project');
-  document.querySelectorAll('.tab').forEach(t=>t.classList.remove('active'));
-  event.target.classList.add('active');
-
-  projects.forEach(p=>{
-    p.style.display=(type==='all'||p.classList.contains(type))?'block':'none';
-  });
-}

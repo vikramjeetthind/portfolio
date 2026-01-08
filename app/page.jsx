3f2dab6d0bd5411306c5f8b1ba93664b8579aae1
Vikramjeet Singh <123534640+vikramjeetthind@users.noreply.github.com>
2026-01-08 23:39:51 +0530

    Create Home component with multiple sections

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import Hero from "@/components/Hero";
+import Services from "@/components/Services";
+import Skills from "@/components/Skills";
+import Portfolio from "@/components/Portfolio";
+import HireMe from "@/components/HireMe";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Services />
+      <Skills />
+      <Portfolio />
+      <HireMe />
+      <Footer />
+    </>
+  );
+}
